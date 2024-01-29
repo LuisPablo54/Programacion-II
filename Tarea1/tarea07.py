ab71c507e03edebccdddb96b10257867fea2adc3
LuisPablo54 <luisirach54@gmail.com>
2024-01-29 10:16:40 -0600

    correccion burbuja

--- Tarea1/tarea07.py
+++ Tarea1/tarea07.py
@@ -2,22 +2,24 @@
 #Luis Pablo López Iracheta 192301-9
 
 def Mediana(lista):
-    if not lista:
-        return None  # Retorna None si la lista está vacía
-    
-    lista_ordenada = sorted(lista)
-    n = len(lista_ordenada)
+    n = len(lista)
+
+    # Ciclo burbuja para ordenar 
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
 
     if n % 2 == 1:
-        # Si la lista tiene un número impar de elementos
-        return lista_ordenada[n // 2]
+        # Si la lista tiene un número impar
+        return lista[n // 2]
     else:
-        # Si la lista tiene un número par de elementos
+        # Si la lista tiene un número par 
         mitad_superior = n // 2
         mitad_inferior = mitad_superior - 1
-        return (lista_ordenada[mitad_inferior] + lista_ordenada[mitad_superior]) / 2
+        return (lista[mitad_inferior] + lista[mitad_superior]) / 2
 
-# Ejemplo de uso:
-valores = [5.5, 3.2, 8.7, 1.1, 7.3, 2.8]
+
+valores = [5.5, 3.2, 8.7, 1.1, 7.3, 2.8, 7.4]
 resultado = Mediana(valores)
 print(resultado)
